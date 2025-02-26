254b687001ad23568519fe8a230c1f552058021e
blacksmith69-glitch <cryptowitharyancourselink@gmail.com>
2025-02-26 19:43:24 +0530

    Update config_tmp.js

--- config/config_tmp.js
+++ config/config_tmp.js
@@ -1,7 +1,7 @@
 {
     "email": "koreacat32@gmail.com",
     "password": "cunp sikw xmyz cosr",
-    "geminiApi": "AIzaSyAjAn2GE9RVr1Pe6nLXwssHhtywWLcX74s",
+    "geminiApi": "AIzaSyAjAn2GE9RVnLXwssHhtywWLcX74s",
     "prompt": "What is the text in this CAPTCHA image? just answer the text",
     "captha2Apikey": "your_apikey_2captcha",
     "apiCaptchakey": "your_api_apicaptcha"
