08a76485e66f1b07a25b342326dcf79698013e5d
blacksmith69-glitch <cryptowitharyancourselink@gmail.com>
2025-05-27 20:28:04 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 {
-    "email": "pakadkejhulja@gmail.com",
-    "password": "mrsy ttt uoa gaye",
+    "email": "cryptowitharyanog@gmail.com",
+    "password": "mrsy ttt uaa gaye",
     "geminiApi": "AIzaSyDONC1YB_JoJWZ9cI",
     "prompt": "What is the text in this CAPTCHA image? just answer the text",
     "captha2Apikey": "your_apikey_2captcha",
